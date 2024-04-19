c99ced0977c69a0b7e4ebcfbcd77bc45b8df9ee6
ThiloWittmer <suppenlp@gmail.com>
2024-04-19 14:15:40 +0200

    form handling

--- src/main/java/de/hsrm/mi/web/app1/BSIController.java
+++ src/main/java/de/hsrm/mi/web/app1/BSIController.java
@@ -1,9 +1,13 @@
 package de.hsrm.mi.web.app1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 
 @Controller
@@ -11,8 +15,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class BSIController {
     
     @GetMapping
-    public String getMethodName(@RequestParam String param) {
+    public String showForm() {
         return "bsi";
     }
     
+    @PostMapping
+    public String postMethodName(Model m, @RequestParam("kGroesse") int kgroesse, @RequestParam("schuhGr") int schuhGr, @RequestParam("semZahl") int semZahl) {
+        int bsiW;
+        float w;
+        w = schuhGr * semZahl;
+        w = kgroesse/w;
+        w *= 100;
+        bsiW = (int) w;
+        m.addAttribute("bsiwert", bsiW);
+        return "bsi";
+    }
+    
+        
 }
